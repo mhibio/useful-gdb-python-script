7af1fdedca426cd79965fdd5bdaaf872390e3c4d
mhibio <46654105+mhibio@users.noreply.github.com>
2022-10-17 02:39:37 +0900

    Update gdb-script.py
    기능추가

--- gdb-script.py
+++ gdb-script.py
@@ -4,8 +4,23 @@ _BYTE = 1
 _DWORD = 4
 _QWORD = 8
 
+def casting(val):
+        return int(val.cast(gdb.lookup_type('unsigned long long')))
+
+def setReg(reg, val):
+    gdb.execute("set ${}={}".format(reg, val))
+
+def setAddr(addr, val, _type=_QWORD):
+    if _type==_QWORD: _type='{long}'
+    elif _type==_DWORD: _type='{int}'
+    elif _type==_BYTE: _type='{char}'
+    gdb.execute("set {}{}={}".format(_type, addr, val))
+
+def getSymbolAddress(sym):
+        return casting(gdb.parse_and_eval(sym).address)
+
 def getReg(register):
-        return gdb.selected_frame().read_register(register)
+        return casting(gdb.selected_frame().read_register(register))
 
 def getQword(address):
     res = gdb.execute("x/gu {}".format(address), to_string=True)
@@ -30,7 +45,7 @@ def Parse2Array(address, _range, _type=_BYTE):
         res.append(func(address+i*_type))
     return res
 
-gdb.execute("file ./a.out")
+gdb.execute("file ./interpreter")
 gdb.execute("start")
 
 addr = 0x5555554009e0
@@ -38,3 +53,13 @@ print(getReg("rsp"))
 print(hex(getQword("$rsp")))
 print(hex(getDword(addr)))
 print(Parse2Array(addr, 0x100, _QWORD))
+
+
+addr = 0x00555555603000 + 0x800
+setAddr(addr, 0xdeadbeefcafebabe, _QWORD)
+print(hex(getQword(addr))) # 0xdeadbeefcafebabe
+
+print(hex(getSymbolAddress('strlen'))) # Strlen Address
+setReg("rax", 0xdedabeefcafebabe)
+print(hex(getReg("rax"))) # 0xdeadbeefcafebabe
+
